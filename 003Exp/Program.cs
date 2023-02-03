00227ae107b2b3df0ae59069db92432b34376da6
Олеся <lisenok56@yandex.ru>
2023-02-03 19:15:02 +0300

    ExpProgAdded

---
+++ 003Exp/Program.cs
@@ -0,0 +1,14 @@
+﻿// Возведите число А в натуральную степень В используя цикл
+
+int num = 2;
+int exp = 6;
+int count = 1;
+int res = num;
+
+while (count < exp)
+{
+    res = res*num;
+    count++;
+}
+
+System.Console.WriteLine($"{num} ^ {exp} = {res} ");
