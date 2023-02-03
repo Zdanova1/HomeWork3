45db6efbc168240312f1d15301a93a7c5affa3d2
Олеся <lisenok56@yandex.ru>
2023-02-03 19:00:47 +0300

    SumProgAdded

---
+++ 002Sum/Program.cs
@@ -0,0 +1,20 @@
+﻿//Найти сумму чисел от 1 до А.
+
+System.Console.Write("Введите число: ");
+int A = Convert.ToInt32(Console.ReadLine());
+
+int GetSum(int A)
+{
+    int num = 1;
+    int sum = 0;
+    for(int i = 0; i<A; i++)
+    {
+            sum = sum + num;
+            num++;
+    }
+    return sum;
+}
+
+int NumSum = GetSum(A);
+System.Console.Write($"Сумма чисел от 1 до А = ");
+System.Console.WriteLine(NumSum);
