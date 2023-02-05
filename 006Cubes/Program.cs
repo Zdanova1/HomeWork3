9f7ef35a608383d0e84c400c4733ffd7603243f1
Олеся <lisenok56@yandex.ru>
2023-02-05 15:59:45 +0300

    CubesProgAdded

---
+++ 006Cubes/Program.cs
@@ -0,0 +1,14 @@
+﻿//Показать кубы чисел, заканчивающихся на четную цифру.
+
+int N = new Random().Next(1,50);
+System.Console.WriteLine($"N = {N}");
+
+int number = 1;
+System.Console.Write("Сube = ");
+while (number<=N)
+{
+        int cube = number*number*number;
+        if (cube % 2 == 0) System.Console.Write(cube + " ");
+        number++;
+}
+System.Console.WriteLine();
