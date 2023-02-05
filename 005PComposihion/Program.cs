35afc63ff37e503a6948c4f58b46fdf9d45b9b5e
Олеся <lisenok56@yandex.ru>
2023-02-05 15:29:43 +0300

    Composotion prog added

---
+++ 005PComposihion/Program.cs
@@ -0,0 +1,16 @@
+﻿// Найти произведение чисел от 1 до N.
+
+System.Console.Write("Введите число: ");
+ int N = Convert.ToInt32(Console.ReadLine());
+
+ int GetNumComp(int x)
+ {
+        int comp = 1;
+        for (int num = 1; num <= x; num++)
+        {
+            comp = comp * num;
+        }
+
+return comp;
+ }
+System.Console.WriteLine($"Произведение чисел от я до {N} равно: "+ GetNumComp(N));
